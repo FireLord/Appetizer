659b8e17641b9f2266fee10ede03f1f86c9db771
FireLord <firelord.xda@gmail.com>
2024-02-26 16:34:43 +0530

    app: Refactor detail view screen

--- Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -29,38 +29,9 @@ struct AppetizerDetailView: View {
                     .padding()
                 
                 HStack(spacing: 40) {
-                    VStack(spacing: 5) {
-                        Text("Calories")
-                            .bold()
-                            .font(.caption)
-                        
-                        Text("\(appetizer.calories)")
-                            .foregroundColor(.secondary)
-                            .fontWeight(.semibold)
-                            .italic()
-                    }
-                    
-                    VStack(spacing: 5) {
-                        Text("Carbs")
-                            .bold()
-                            .font(.caption)
-                        
-                        Text("\(appetizer.carbs)")
-                            .foregroundColor(.secondary)
-                            .fontWeight(.semibold)
-                            .italic()
-                    }
-                    
-                    VStack(spacing: 5) {
-                        Text("Protein")
-                            .bold()
-                            .font(.caption)
-                        
-                        Text("\(appetizer.protein)")
-                            .foregroundColor(.secondary)
-                            .fontWeight(.semibold)
-                            .italic()
-                    }
+                    NutritionInfo(title: "Calories", value: appetizer.calories)
+                    NutritionInfo(title: "Carbs", value: appetizer.carbs)
+                    NutritionInfo(title: "Protein", value: appetizer.protein)
                 }
             }
             
@@ -69,13 +40,7 @@ struct AppetizerDetailView: View {
             Button {
                 print("hi")
             } label: {
-                Text("$\(appetizer.price, specifier: "%.2f") - Add to Order")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .frame(width: 260, height: 50)
-                    .background(Color.brandPrimary)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+                APButton(title: "$\(appetizer.price, specifier: "%.2f") - Add to Order")
             }
             .padding(.bottom, 30)
         }
@@ -87,17 +52,7 @@ struct AppetizerDetailView: View {
             Button {
                 isShowingDetail.toggle()
             } label: {
-                ZStack {
-                    Circle()
-                        .frame(width: 30, height: 30)
-                        .foregroundColor(.white)
-                        .opacity(0.6)
-                    
-                    Image(systemName: "xmark")
-                        .imageScale(.small)
-                        .frame(width: 44, height: 44)
-                        .foregroundColor(.black)
-                }
+                XDismissButton()
             },
             alignment: .topTrailing)
     }
@@ -106,4 +61,22 @@ struct AppetizerDetailView: View {
 #Preview {
     AppetizerDetailView(appetizer: MockData.sampleAppetizer,
                         isShowingDetail: .constant(true))
+}
+
+struct NutritionInfo: View {
+    let title: String
+    let value: Int
+    
+    var body: some View {
+        VStack(spacing: 5) {
+            Text(title)
+                .bold()
+                .font(.caption)
+            
+            Text("\(value)")
+                .foregroundColor(.secondary)
+                .fontWeight(.semibold)
+                .italic()
+        }
+    }
 }
---
+++ Appetizers/Views/Buttons/APButton.swift
@@ -0,0 +1,26 @@
+//
+//  APButton.swift
+//  Appetizers
+//
+//  Created by admin on 26/02/24.
+//
+
+import SwiftUI
+
+struct APButton: View {
+    let title: LocalizedStringKey
+    
+    var body: some View {
+        Text("Test Button")
+            .font(.title2)
+            .fontWeight(.semibold)
+            .frame(width: 260, height: 50)
+            .background(Color.brandPrimary)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+}
+
+#Preview {
+    APButton(title: "Test Button")
+}
