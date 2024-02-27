eeb73cd5d57c53737fcaeb49e59d81addf351af1
FireLord <firelord.xda@gmail.com>
2024-02-27 17:42:55 +0530

    app: Add error handling of api

--- Appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
@@ -51,6 +51,18 @@ import SwiftUI
                 appetizers = try await NetworkManager.shared.getAppetizers()
                 isLoading = false
             } catch {
+                if let apError = error as? APError {
+                    switch apError {
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
+                }
                 alertItem = AlertContext.invalidResponse
                 isLoading = false
             }
